42618f0264bfb403ffa82ba01de31decacf24d6f
Carlos Morales <40382318+Pseudo-codigo@users.noreply.github.com>
2024-03-14 21:42:52 -0600

    +Github Actions Pages Config
    .
    
    .
    
    test
    
    .
    
    ..
    
    s
    
    test
    
    test
    
    test
    
    .
    
    .
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    .
    
    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-    output: 'standalone',
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    basePath: isProd ? '/react-challenge' : undefined,
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "React Challange",
+  title: "React Challenge",
   description: "Developed by Carlos Morales",
 };
 
